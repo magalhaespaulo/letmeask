8f799098be0a86331cf15531d35e03fff6c104b8
Paulo Magalhães <pauloaxn@gmail.com>
2021-10-20 15:07:35 -0300

    add Styleguide from Figma to Tailwind CSS

---
+++ src/pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static getInitialProps = async (
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> => {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap" // swap|optional
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,54 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent:      'transparent',
+      white: {
+        DEFAULT:        '#FEFEFE',
+        'dark':         '#F8F8F8',
+      },
+      black: {
+        DEFAULT:        '#29292E',
+        'dark':         '#050206',
+      },
+      gray: {
+        'light':        '#DBDCDD',
+        'medium':       '#A8A8B3',
+        'dark':         '#737380',
+      },
+      purple:           '#835AFD',
+      danger:           '#E73F5D',
+      pink: {
+        'light':        '#D67EE2',
+        'dark':         '#E559F9',
+      },
+      hover: {
+        'purple':       '#6F4BD8',
+        'danger':       '#D73754',
+        'gray-medium':  '#7E7E86',
+        'gray-light':   '#CECECE',
+      }
+    },
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+      'poppins': ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      flex: {
+        '2': '2 2 0%',
+        '3': '3 3 0%',
+        '4': '4 4 0%',
+        '5': '5 5 0%',
+        '6': '6 6 0%',
+        '7': '7 7 0%',
+      }
+    },
   },
   variants: {
     extend: {},
