b050deb112557ab5875122fde73a1d02ea6c13b4
Paulo Magalhães <pauloaxn@gmail.com>
2021-10-23 00:03:24 -0300

    add signIn on Room

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -19,7 +19,7 @@ import { Question } from '../../components/Question'
 import { SpinnerSVG } from '../../components/SpinnerSVG'
 
 const Room: NextPage = () => {
-  const { user } = useAuth()
+  const { user, signInWithGoogle } = useAuth()
   const router = useRouter()
   const { id } = router.query
   const roomId = id as string
@@ -29,6 +29,7 @@ const Room: NextPage = () => {
 
   const { animate, setAnimate } = useAnimate()
   const [loading, setLoading] = useState(false)
+  const [loadingGoogle, setLoadingGoogle] = useState(false)
 
   const handleSendQuestion = async (event: FormEvent) => {
     event.preventDefault()
@@ -140,11 +141,21 @@ const Room: NextPage = () => {
                 </div>
               </div>
             ) : (
-              <span className="text-gray-dark text-sm font-medium">
+              <span
+                className="
+                  flex items-center
+                  text-gray-dark text-sm font-medium"
+              >
                 Para enviar uma pergunta,
-                <button className="line-link ml-1 text-purple border-purple font-medium">
+                <button
+                  className="line-link ml-1 text-purple border-purple font-medium"
+                  onClick={(event) => signIn(event)}
+                >
                   faça seu login
                 </button>
+                {loadingGoogle && (
+                  <SpinnerSVG className="-ml-r ml-3 text-purple" />
+                )}
               </span>
             )}
             <Button type="submit" disabled={!user}>
@@ -170,34 +181,33 @@ const Room: NextPage = () => {
                     {question.likeCount}
                   </span>
                 )}
-                {!question.isAnswered && (
-                  <button
-                    className={`
-                      flex items-center justify-center
-                      w-10 h-8
-                      hover-animation
-                      ${question.likeId ? 'text-purple' : 'text-gray-dark'}`}
-                    aria-label="Gostei"
-                    onClick={() =>
-                      handleLikeQuestion(question.id, question.likeId)
-                    }
+                <button
+                  className={`
+                    flex items-center justify-center
+                    w-10 h-8
+                    hover-animation
+                    ${question.likeId ? 'text-purple' : 'text-gray-dark'}`}
+                  aria-label="Gostei"
+                  onClick={() =>
+                    handleLikeQuestion(question.id, question.likeId)
+                  }
+                  disabled={!user || question.isAnswered}
+                >
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
                   >
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </button>
-                )}
+                    <path
+                      d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
               </div>
             </Question>
           )
